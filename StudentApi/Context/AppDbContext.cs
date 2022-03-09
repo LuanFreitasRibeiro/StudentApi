2ab73bf01e791f8f8b182822aa437ea597d75c8d
Luan Freitas <luannr16@gmail.com>
2022-03-09 19:44:01 -0300

    initial commit

---
+++ StudentApi/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApi.Context.Map;
+using StudentApi.Models;
+
+namespace StudentApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new StudentMap());
+        }
+    }
+}
---
+++ StudentApi/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+    }
+}
